0d0345bf3b52b56bdb3aca55cd08631a58dd8573
Aadil Hassan <68943818+Aadilhassan@users.noreply.github.com>
2022-12-04 03:43:16 +0530

    partial navbar

--- pages/api/trending.js
+++ pages/api/trending.js
@@ -3,7 +3,7 @@
 export default async function handler(req, res) {
    let baseUrl = 'https://www.googleapis.com/youtube/v3/'
 
-  fetch(baseUrl + 'videos?part=snippet,statistics&chart=mostPopular&maxResults=50&key=AIzaSyDjyzVyxBNNI7g1gziV4nIAqaaC4S5ySkI')
+  fetch(baseUrl + `videos?part=snippet,statistics&chart=mostPopular&maxResults=50&key=${process.env.YOUTUBE_API_KEY}`)
       .then(response => response.json())
       .then(data => {
             res.status(200).json(data.items)
