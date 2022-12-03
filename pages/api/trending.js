401dbef6ab2f21b9c7298292e34c37775d75aa64
Aadil Hassan <68943818+Aadilhassan@users.noreply.github.com>
2022-12-04 00:57:36 +0530

    api endPoint /trending with docs

--- pages/api/trending.js
+++ pages/api/trending.js
@@ -1,6 +1,15 @@
 
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+   let baseUrl = 'https://www.googleapis.com/youtube/v3/'
 
-   res.status(200).json({data: "youtube"})
+  fetch(baseUrl + 'videos?part=snippet&chart=mostPopular&maxResults=50&key=AIzaSyDjyzVyxBNNI7g1gziV4nIAqaaC4S5ySkI')
+      .then(response => response.json())
+      .then(data => {
+            res.status(200).json(data)
+      })
+   // const data = await axios.get('https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDjyzVyxBNNI7g1gziV4nIAqaaC4S5ySkI')
+
+
+
 }
